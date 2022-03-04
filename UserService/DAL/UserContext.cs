3a873ecc71b211218be130a4ef9994b4322f6256
Bakker,Sebas S <sebasbakker8@hotmail.com>
2022-03-04 16:12:47 +0100

    Created basic user seeding database

--- UserService/DAL/UserContext.cs
+++ UserService/DAL/UserContext.cs
@@ -6,18 +6,18 @@ namespace UserService.DAL
 {
     public class UserContext : DbContext
     {
+        public UserContext(DbContextOptions<UserContext> options): base(options)
+        {
+        }
         public DbSet<User> Users { get; set; }
         //public DbSet<Followers> Followers { get; set; }
         //public DbSet<Following> Following { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySQL("server=localhost;database=kwetter;user=root;password=");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("User");
             modelBuilder.Entity<User>(entity =>
             {
                 entity.HasKey(e => e.Id);
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,13 +1,42 @@
 ﻿using UserService.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace UserService
-{ 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            UserInitializer userInit = new UserInitializer();
-            userInit.SeedData();
-        }
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UserContext>(options =>
+  options.UseMySQL("server=localhost;database=kwetter;user=root;password="));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<UserContext>();
+    context.Database.EnsureCreated();
+    UserInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+
+app.Run();
